f59bd0975664987848d01abd3e5dda66ff23355e
PikoLab <nonie.115@gmail.com>
2021-07-10 12:59:37 +0800

    first version

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: process.env.CLARIFAI_API
+    apiKey: ' '  /* put your api key */
    });
 
 const handleApiCall=(req,res)=>{
--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const image=require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
-        }
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'aibrain'
     }
   });
 
@@ -34,6 +34,6 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(process.env.PORT ||3000, ()=>{
-    console.log(`app is running on ${process.env.PORT}`);
+app.listen(3000, ()=>{
+    console.log('app is running on 3000');
 })
